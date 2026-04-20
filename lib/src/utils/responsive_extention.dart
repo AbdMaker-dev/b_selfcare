12251ff2184a3726e0fdd28dbd1923c9cec84a88
AbdMaker-dev <dioufbadaraalioune7@gmail.com>
2026-04-20 12:13:35 +0000

    optimise dashboard ui

--- lib/src/utils/responsive_extention.dart
+++ lib/src/utils/responsive_extention.dart
@@ -3,12 +3,12 @@ import 'package:flutter/foundation.dart';
 
 extension ResponsiveExtension on num {
   // Dimensions - avec facteur web
-  double get rh => kIsWeb ? h * 1.2 : h;
+  double get rh => kIsWeb ? h : h;
   double get rw => kIsWeb ? w  : w;
   
   // Texte & Icons - maintenir la lisibilité
   double get rsp => kIsWeb ? sp  : sp; // Pas de modification
   
   // Radius - proportionnel aux dimensions
-  double get rr => kIsWeb ? r * 0.8 : r;
+  double get rr => kIsWeb ? r : r;
 }
