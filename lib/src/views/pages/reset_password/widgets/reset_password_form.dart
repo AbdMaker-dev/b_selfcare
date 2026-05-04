a0216cdeeffa98775cc02bc701d3d51ffcf67651
Ousman Adda NAPAL <ousman-adda.napal@free-partenaires.sn>
2026-05-04 17:53:07 +0000

    on est en refacto

--- lib/src/views/pages/reset_password/widgets/reset_password_form.dart
+++ lib/src/views/pages/reset_password/widgets/reset_password_form.dart
@@ -22,75 +22,85 @@ class ResetPasswordForm extends StatefulWidget {
 }
 
 class _ResetPasswordFormState extends State<ResetPasswordForm> {
-  final resetCubit = getIt<ResetPasswordCubit>();
   final TextEditingController _emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _submit() async{
+    if(!_formKey.currentState!.validate()) return;
+    final resetCubit = getIt<ResetPasswordCubit>();
+    await resetCubit.forgetPassword(email: _emailController.text.trim());
+  }
 
   @override
   Widget build(BuildContext context) {
     final s = S.of(context);
-    return BlocConsumer<ResetPasswordCubit, ResetPasswordState>(
-      bloc: resetCubit,
-      listener: (context, state) {
-        state.maybeWhen(
-            resetPasswordFailed: (message){
-            },
-            resetPasswordError: (data){
-            },
-            orElse: (){}
-        );
-      },
-      builder: (context, state) {
-        return state is ResetPasswordLoaded ?
-        SendMailMessage(
-          emailSentDescription: s.emailSentDescription,
-          emailSentTitle:  s.emailSentTitle,
-          icon: Icons.mark_email_read_outlined,
-          isNotSend: true,
-        ):
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AppText.textHighlight(
-              "${s.titlereset}\n${s.pwd}",
-              highlight: s.pwd,
-              highlightHeight: 0.68,
-              fontSize: 46.rsp,
-              highlightFontSize: 46.rsp,
-              color: AppColors.primary,
-              fontWeight: FontWeight.w900,
-              highlightColor: AppColors.secondary,
-              fontStyle: FontStyle.italic,
-              textAlign: TextAlign.center
-            ),
-            SizedBox(height: 40.0.rh),
-            AppInput(
-              labelText: s.email,
-              keyboardType: TextInputType.emailAddress,
-              controller: _emailController,
-            ),
-            SizedBox(height: 50.0.rh),
-            AppButton(
-              text: s.resetPwd,
-              onPressed: () {
-                resetCubit.forgetPassword(email: _emailController.text);
-                //context.router.replaceAll([ChangePasswordRoute()]);
-              },
-              type: AppButtonType.primary,
-              icon: Icons.arrow_forward,
-            ),
-            SizedBox(height: 30.0.rh),
-            AppButton(
-              text: s.login,
-              onPressed: () {
-                context.router.replaceAll([LoginRoute()]);
-              },
-              type: AppButtonType.outline,
-              icon: Icons.arrow_back,
-            ),
-          ],
-        );
-      },
+    return Form(
+      key: _formKey,
+      child: BlocConsumer<ResetPasswordCubit, ResetPasswordState>(
+        bloc: getIt<ResetPasswordCubit>(),
+        listener: (context, state) {
+          state.maybeWhen(
+            resetPasswordFailed: (message) {},
+            resetPasswordError: (data) {},
+            orElse: () {},
+          );
+        },
+        builder: (context, state) {
+          return state is ResetPasswordLoaded
+              ? SendMailMessage(
+                  emailSentDescription: s.emailSentDescription,
+                  emailSentTitle: s.emailSentTitle,
+                  icon: Icons.mark_email_read_outlined,
+                  isNotSend: true,
+                )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    AppText.textHighlight(
+                      "${s.titlereset}\n${s.pwd}",
+                      highlight: s.pwd,
+                      highlightHeight: 0.68,
+                      fontSize: 46.rsp,
+                      highlightFontSize: 46.rsp,
+                      color: AppColors.primary,
+                      fontWeight: FontWeight.w900,
+                      highlightColor: AppColors.secondary,
+                      fontStyle: FontStyle.italic,
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 40.0.rh),
+                    AppInput(
+                      labelText: s.email,
+                      keyboardType: TextInputType.emailAddress,
+                      controller: _emailController,
+                    ),
+                    SizedBox(height: 50.0.rh),
+                    AppButton(
+                      text: s.resetPwd,
+                      onPressed: _submit,
+                      type: AppButtonType.primary,
+                      icon: Icons.arrow_forward,
+                    ),
+                    SizedBox(height: 30.0.rh),
+                    AppButton(
+                      text: s.login,
+                      onPressed: () {
+                        context.router.replaceAll([LoginRoute()]);
+                      },
+                      type: AppButtonType.outline,
+                      icon: Icons.arrow_back,
+                    ),
+                  ],
+                );
+        },
+      ),
     );
   }
 }
