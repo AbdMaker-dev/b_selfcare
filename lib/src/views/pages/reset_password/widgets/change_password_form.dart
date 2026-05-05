c6cfa60f6db6d7e8d3aa4ca44d9aa6d8ff59ab1b
Ousman Adda NAPAL <napalousmanadda@gmail.com>
2026-05-05 12:08:24 +0000

    update gestion input

--- lib/src/views/pages/reset_password/widgets/change_password_form.dart
+++ lib/src/views/pages/reset_password/widgets/change_password_form.dart
@@ -29,86 +29,97 @@ class ChangePasswordForm extends StatefulWidget {
 }
 
 class _ChangePasswordFormState extends State<ChangePasswordForm> {
-  final resetCubit = getIt<ResetPasswordCubit>();
   final TextEditingController _newPasswordController = TextEditingController();
-  final TextEditingController _confirmPasswordController =
-      TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _newPasswordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _submit() async {
+    if (!_formKey.currentState!.validate()) return;
+    final resetCubit = getIt<ResetPasswordCubit>();
+    await resetCubit.resetPassword(email: widget.email,
+        password: _newPasswordController.text.trim(),
+        passwordConfirmation:_confirmPasswordController.text.trim(),
+        token: widget.token);
+  }
 
   @override
   Widget build(BuildContext context) {
     final s = S.of(context);
-    return BlocConsumer<ResetPasswordCubit, ResetPasswordState>(
-      bloc: resetCubit,
-      listener: (context, state) {
-        state.maybeWhen(
-          changePasswordFailed: (message) {},
-          changePasswordError: (data) {},
-          orElse: () {},
-        );
-      },
-      builder: (context, state) {
-        return state is ChangePasswordLoaded ?
-        SendMailMessage(
-          emailSentDescription: state.data.message ?? "",
-          emailSentTitle:  s.pwdChangement,
-          icon: Icons.mark_chat_read_sharp,
-        ) :
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AppText.textHighlight(
-              "${s.titlereset}\n${s.newPwd}",
-              highlight: s.newPwd,
-              highlightHeight: 0.68,
-              fontSize: 46.rsp,
-              highlightFontSize: 46.rsp,
-              color: AppColors.primary,
-              fontWeight: FontWeight.w900,
-              highlightColor: AppColors.secondary,
-              fontStyle: FontStyle.italic,
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 40.0.rh),
-            AppInput(
-              labelText: s.newPassword,
-              keyboardType: TextInputType.text,
-              isPassword: true,
-              controller: _newPasswordController,
-            ),
-            SizedBox(height: 20.rh),
-            AppInput(
-              labelText: s.confirmPassword,
-              keyboardType: TextInputType.text,
-              controller: _confirmPasswordController,
-              isPassword: true,
-            ),
-            SizedBox(height: 50.0.rh),
-            AppButton(
-              text: s.resetPwd,
-              onPressed: () {
-                resetCubit.resetPassword(
-                  email: widget.email,
-                  password: _newPasswordController.text,
-                  passwordConfirmation: _confirmPasswordController.text,
-                  token: widget.token,
-                );
-              },
-              type: AppButtonType.primary,
-              icon: Icons.arrow_forward,
-            ),
-            SizedBox(height: 30.0.rh),
-            AppButton(
-              text: s.login,
-              onPressed: () {
-                context.router.replaceAll([LoginRoute()]);
-              },
-              type: AppButtonType.outline,
-              icon: Icons.arrow_back,
-            ),
-          ],
-        );
-      },
+    return Form(
+      key: _formKey,
+      child: BlocConsumer<ResetPasswordCubit, ResetPasswordState>(
+        bloc: getIt<ResetPasswordCubit>(),
+        listener: (context, state) {
+          state.maybeWhen(
+            changePasswordFailed: (message) {},
+            changePasswordError: (data) {},
+            orElse: () {},
+          );
+        },
+        builder: (context, state) {
+          return state is ChangePasswordLoaded ?
+          SendMailMessage(
+            emailSentDescription: state.data.message ?? "",
+            emailSentTitle: s.pwdChangement,
+            icon: Icons.mark_chat_read_sharp,
+          ) :
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AppText.textHighlight(
+                "${s.titlereset}\n${s.newPwd}",
+                highlight: s.newPwd,
+                highlightHeight: 0.68,
+                fontSize: 46.rsp,
+                highlightFontSize: 46.rsp,
+                color: AppColors.primary,
+                fontWeight: FontWeight.w900,
+                highlightColor: AppColors.secondary,
+                fontStyle: FontStyle.italic,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 40.0.rh),
+              AppInput(
+                labelText: s.newPassword,
+                keyboardType: TextInputType.text,
+                isPassword: true,
+                controller: _newPasswordController,
+              ),
+              SizedBox(height: 20.rh),
+              AppInput(
+                labelText: s.confirmPassword,
+                keyboardType: TextInputType.text,
+                controller: _confirmPasswordController,
+                isPassword: true,
+              ),
+              SizedBox(height: 50.0.rh),
+              AppButton(
+                text: s.resetPwd,
+                onPressed: _submit,
+                type: AppButtonType.primary,
+                icon: Icons.arrow_forward,
+              ),
+              SizedBox(height: 30.0.rh),
+              AppButton(
+                text: s.login,
+                onPressed: () {
+                  context.router.replaceAll([LoginRoute()]);
+                },
+                type: AppButtonType.outline,
+                icon: Icons.arrow_back,
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
