d0657b445eeec5e1881ce420158b01d943ce5964
Ousman Adda NAPAL <napalousmanadda@gmail.com>
2026-05-18 09:46:04 +0000

    correction campagne

--- lib/src/data/models/campaign/data_campaign_response_model.dart
+++ lib/src/data/models/campaign/data_campaign_response_model.dart
@@ -1,37 +1,25 @@
-import 'package:b_selfcare/src/data/models/campaign/campaign_model.dart';
-import 'package:b_selfcare/src/data/models/meta_model.dart';
+import 'data_campaign_model.dart';
 
 class DataCampaignResponseModel {
   bool? success;
   String? message;
-  MetaModel? meta;
-  List<CampaignModel>? campaigns;
+  DataCampaignModel? data;
 
-  DataCampaignResponseModel({this.success, this.message,this.meta,this.campaigns});
+  DataCampaignResponseModel({this.success, this.message, this.data});
 
   DataCampaignResponseModel.fromJson(Map<String, dynamic> json) {
     success = json['success'];
     message = json['message'];
-    meta = json['meta'] != null ? MetaModel.fromJson(json['meta']) : null;
-    if (json['data'] != null) {
-      campaigns = <CampaignModel>[];
-      json['data'].forEach((v) {
-        campaigns!.add(CampaignModel.fromJson(v));
-      });
-    }
+    data = json['data'] != null ? DataCampaignModel.fromJson(json['data']) : null;
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> map = {};
     map['success'] = success;
     map['message'] = message;
-    if (meta != null) {
-      map['meta'] = meta!.toJson();
-    }
-    if (campaigns != null) {
-      map['data'] = campaigns!.map((v) => v.toJson()).toList();
+    if (data != null) {
+      map['data'] = data!.toJson();
     }
     return map;
   }
 }
-
